b57da4c6054d0a6e1c3ab5922638e2686cc8952c
Agga <hanzawnyein@Hans-MacBook-Air.local>
2024-10-22 21:34:52 +0700

    add ica course

--- addons/ica_elearning/models/ica_course.py
+++ addons/ica_elearning/models/ica_course.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class Course(models.Model):
@@ -12,8 +13,30 @@ class Course(models.Model):
         ('cancelled', 'Cancelled'),
     ], default='draft')
     partner_id = fields.Many2one('res.partner', required=True)
-    category_ids = fields.Many2many('res.partner.category')
+    category_ids = fields.Many2many('res.partner.category', copy=False)
     company_id = fields.Many2one('res.company', default=lambda self: self.env.company, readonly=True)
     currency_id = fields.Many2one('res.currency', related="company_id.currency_id")
     fees = fields.Monetary()
     lesson_ids = fields.One2many('ica.lesson', 'course_id', domain=[('state', '=', 'published')])
+    active = fields.Boolean(default=True)
+
+    def action_draft(self):
+        self.state = 'draft'
+
+    def action_published(self):
+        if len(self.lesson_ids) <= 0:
+            raise UserError(_('You must select at least one lesson'))
+        self.state = 'published'
+
+    def action_cancelled(self):
+        self.state = 'cancelled'
+
+    # def action_add_lesson(self):
+    #     return {
+    #         'type': 'ir.actions.act_window',
+    #         "name": "Add Lesson",
+    #         "res_model": "ica.lesson",
+    #         "view_mode": "form",
+    #         "target": "new",
+    #         "context":{"default_course_id":self.id}
+    #     }
